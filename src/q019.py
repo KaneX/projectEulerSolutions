8a22b02fc5e2ae9d097a360b78797a167fd1b1ee
Jiming Xu <xujimingkane@gmail.com>
2016-08-29 18:06:14 -0230

    Q019: submitted

--- src/q019.py
+++ src/q019.py
@@ -3,6 +3,13 @@ import math
 
 
 def zeller(y, m, d):
+    """
+    Zeller's congruence
+    :param y: year
+    :param m: month
+    :param d: day
+    :return: current date of the week, 1 for Monday, 2 for Tuesday ...
+    """
     if m < 3:
         m += 12
         y -= 1
@@ -16,12 +23,10 @@ for i in range(400):
     for j in range(1, 13):
         if i == 0 and j == 1:
             continue
-        if zeller(i+1900, j, 1) == 1:
+        if zeller(i+1900, j, 1) == 7:
             matchSumList[i][j-1] = matchSumList[i][j-2] + 1 if j > 1 else matchSumList[i-1][11] + 1
         else:
             matchSumList[i][j-1] = matchSumList[i][j-2] if j > 1 else matchSumList[i-1][11]
-
-cntIn400Yrs = 684
 
 T = int(input().strip())
 
@@ -29,15 +34,15 @@ for a0 in range(T):
     Y1, M1, D1 = map(int, input().split())
     Y2, M2, D2 = map(int, input().split())
 
-    cnt1 = (Y1 - 1900) // 400 * cntIn400Yrs
+    cnt1 = (Y1 - 1900) // 400 * matchSumList[399][11]
     Y1 -= (Y1 - 1900) // 400 * 400
     cnt1 += matchSumList[Y1 - 1900][M1 - 1]
-
-    if zeller(Y1, M1, D1) == 1:
+    print(Y1)
+    if zeller(Y1, M1, D1) == 7:
         cnt1 -= 1
 
-    cnt2 = (Y2 - 1900) // 400 * cntIn400Yrs
+    cnt2 = (Y2 - 1900) // 400 * matchSumList[399][11]
     Y2 -= (Y2 - 1900) // 400 * 400
     cnt2 += matchSumList[Y2 - 1900][M2 - 1]
-
+    print(Y2)
     print(cnt2 - cnt1)
