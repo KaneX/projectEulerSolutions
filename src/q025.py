090843bc05a2f64429bd12d6094f0f53efce7e93
Jiming Xu <xujimingkane@gmail.com>
2016-09-05 17:39:53 -0230

    Q025: transform the formula to optimize it to O(1) complexity

--- src/q025.py
+++ src/q025.py
@@ -6,19 +6,10 @@ Golden = (1 + 5 ** 0.5) / 2
 preComp1 = math.log10(Golden)
 preComp2 = 1 - math.log10(5 ** 0.5)
 
-firstEncounterList = [0, 1]
-i = 1
-while len(firstEncounterList) <= 5000:
-    i += 1
-    # use a formula to compute the length of Fibonacci numbers
-    # ref: http://www.had2know.com/academics/number-digits-length-fibonacci-number.html
-    if math.floor(i * preComp1 + preComp2) >= len(firstEncounterList):
-        firstEncounterList.append(i)
-
-# print(firstEncounterList)
-
 T = int(input().strip())
 
 for a0 in range(T):
     N = int(input().strip())
-    print(firstEncounterList[N])
+    # use a formula to compute the length of Fibonacci numbers
+    # ref: http://www.had2know.com/academics/number-digits-length-fibonacci-number.html
+    print(math.ceil((N - preComp2) / preComp1))
