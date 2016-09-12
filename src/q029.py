af38beaa36bca0c06a5a9b530a7729e60e4d2a37
Jiming Xu <xujimingkane@gmail.com>
2016-09-12 18:59:01 -0230

    Q027: optimized by computing the powers

--- src/q029.py
+++ src/q029.py
@@ -1,9 +1,25 @@
-l = []
 size = int(input().strip())
-for i in range(2, size+1):
-    for j in range(2, size+1):
-        n = i**j
-        if n not in l:
-            l.append(n)
+num = 0
+isComputed = [False for x in range(0, size+1)]
 
-print(len(l))
+
+for i in range(2, size+1):
+    if isComputed[i]:
+        continue
+    val = set()
+    base = 1
+    for j in range(1, 15):    # 2**14 > 100000
+        base *= i
+        if base > size:
+            break
+        isComputed[base] = True
+        for k in range(2, size+1):
+            n = j * k
+
+            if n not in val:
+                val.add(n)
+                num += 1
+
+    print(val)
+
+print(num)
