5a9692ac29e65361bb90df87640bd1fea234754a
Jiming Xu <xujimingkane@gmail.com>
2016-09-12 19:18:20 -0230

    Q027: submitted

--- src/q029.py
+++ src/q029.py
@@ -1,25 +1,34 @@
-size = int(input().strip())
-num = 0
-isComputed = [False for x in range(0, size+1)]
+import sys
+import math
 
+MaxPower = int(math.log2(10**5)) + 2
+
+size = int(input().strip())
+
+num = 0
+isComputed = [False] * (size + 1)
+numList = [0] * MaxPower
+
+# precompute all power possibilities for the same base, the index is the max power < 100000 we can get
+# using this base
+val = set()
+for j in range(1, MaxPower):
+    for i in range(2, size+1):
+        val.add(j * i)
+    numList[j] = len(val)
 
 for i in range(2, size+1):
     if isComputed[i]:
         continue
     val = set()
     base = 1
-    for j in range(1, 15):    # 2**14 > 100000
+    j = 0
+    while j < MaxPower:
+        j += 1
         base *= i
         if base > size:
             break
         isComputed[base] = True
-        for k in range(2, size+1):
-            n = j * k
-
-            if n not in val:
-                val.add(n)
-                num += 1
-
-    print(val)
+    num += numList[j-1]
 
 print(num)
